535bb9028ae741c22e77c5044f701ba270c9d874
IanOlin <ian.paul@students.olin.edu>
2017-03-31 16:21:36 -0400

    something idk

--- graphs/day02/src/Permutations.java
+++ graphs/day02/src/Permutations.java
@@ -1,10 +1,41 @@
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Arrays;
 
 public class Permutations {
 
     public static List<List<Integer>> permutations(List<Integer> A) {
-        // TODO
-        return null;
+        List<List<Integer>> perms = new ArrayList();
+//        List<Integer> P = new ArrayList<>();
+//        List<Integer> a = new LinkedList<>();
+//
+//        permsHelp(A, P);
+        return perms;
     }
+
+    public static List<List<Integer>> permsHelp2 (List<Integer> A, List<List<Integer>> P){
+        if( A.size() == 0){
+
+        }
+    }
+
+//    public static List<Integer> permsHelp(int A[], List<Integer> P){
+//        //A isn't mutable. Fuck this
+//        if (A.length == 0){
+//            System.out.println(P);
+//            return P;
+//        }
+//        else {
+//            for (int i = 0; i < A.length; i++) {
+//                P.add(A[i]);
+//                A = ArrayUtil
+//                permsHelp(A, P);
+//            }
+//        }
+//        return null;
+//    }
 
 }
